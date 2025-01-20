a7faedfb240e438ad89f6aef1c16af07e4e5c9ed
ljs9904ljs <57557223+ljs9904ljs@users.noreply.github.com>
2025-01-20 22:31:21 +0900

    '백준' '합이 0' '골드4' 문제를 제대로 풀지 못했음. c++로는 풀리는 아이디어가 python에서는 시간 초과임. 다른 사람의 풀이를 보고 아이디어를 얻어서 투 포인터 기법을 적용하였다.

---
+++ 백준/바킹독의-실전-알고리즘/이분탐색/11-3151-합이0-골드4.py
@@ -0,0 +1,54 @@
+"""
+
+혼자서 못 풀어서 다른 사람 풀이를 보고 재구성하는데 2시간 정도 걸렸다.
+
+채점 환경에 따라 실행 속도 차이로 정답/오답 갈리는 경우가 있는 것 같다.
+pypy3로 통과하는 코드가 python3에서는 실패한다.
+c++에서 통과하는 로직을 그대로 python3로 구현하였더니 시간 초과로 실패한다.
+
+값이 겹치는 경우를 고민해볼 수 있는 문제이다.
+
+"""
+
+
+import sys
+from bisect import bisect_left, bisect_right
+
+
+input = sys.stdin.readline
+
+
+
+n = int(input())
+arr = list(map(int, input().split()))
+# n = 10
+# arr = list(map(int,"2 -5 2 3 -4 7 -4 0 1 -6".split()))
+#arr = [-6, -5, -4, -4, 0, 1, 2, 2, 3, 3, 3, 7]
+
+arr.sort()
+
+
+cnt = 0
+
+for i in range(n - 2):
+    l = i + 1
+    r = n - 1
+
+    while l < r:
+        v = arr[i] + arr[l] + arr[r]
+
+        if v == 0:
+            if arr[l] == arr[r]:
+                cnt += r - l
+            else:
+                cnt += r - bisect_left(arr, arr[r], lo=l) + 1
+            l += 1
+        elif v < 0:
+            l += 1
+        else:
+            r -= 1
+
+
+
+print(cnt)
+
