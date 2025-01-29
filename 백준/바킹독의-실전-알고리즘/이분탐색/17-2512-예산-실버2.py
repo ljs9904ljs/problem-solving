510161447e3c10e7149f4279534d40c8a91f8111
ljs9904ljs <57557223+ljs9904ljs@users.noreply.github.com>
2025-01-29 18:12:22 +0900

    '백준' '예산' '실버2' 문제를 풀었음.

---
+++ 백준/바킹독의-실전-알고리즘/이분탐색/17-2512-예산-실버2.py
@@ -0,0 +1,44 @@
+"""
+
+문제 푸는 데 걸린 시간: 9분 32초
+
+"""
+
+
+
+import sys
+
+
+input = sys.stdin.readline
+
+
+n = int(input())
+arr = list(map(int, input().split()))
+total = int(input())  # 총 예산
+
+
+def check(limit: int) -> bool:
+    cur_budget = sum([min(num, limit) for num in arr])
+    return cur_budget <= total
+
+
+if sum(arr) <= total:
+    print(max(arr))
+else:
+    l = 0
+    r = total
+
+    ans = None
+    while l <= r:
+        mid = (l + r) // 2
+        
+        if check(mid):
+            ans = mid
+            l = mid + 1
+        else:
+            r = mid - 1
+            
+
+    print(ans)
+
+        
