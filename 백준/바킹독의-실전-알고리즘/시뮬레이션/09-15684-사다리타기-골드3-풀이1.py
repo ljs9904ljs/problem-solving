98536dfcf27bfe26169bef77861fb2e971c288c9
ljs9904ljs <57557223+ljs9904ljs@users.noreply.github.com>
2025-01-31 22:11:02 +0900

    '백준' '사다리 타기' '골드3' 문제를 다른 풀이의 도움을 받아 풀었음. 초기 사다리의 위치 배열을 이용한다는 아이디어를 떠올리지 못했었음.

---
+++ 백준/바킹독의-실전-알고리즘/시뮬레이션/09-15684-사다리타기-골드3-풀이1.py
@@ -0,0 +1,131 @@
+
+
+
+import sys
+import copy
+
+
+input = sys.stdin.readline
+
+n, m, h = map(int, input().split())
+
+ladder = [[False] * (n + 5) for _ in range(h + 5)]
+ladder_copy = [[False] * (n + 5) for _ in range(h + 5)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    
+    ladder[a][b] = True  # 가로선이 놓였다.
+    ladder_copy[a][b] = True
+
+choices = []
+
+for i in range(1, h + 1):
+    for j in range(1, n):
+        if ladder[i][j - 1] or ladder[i][j] or ladder[i][j + 1]:
+            continue
+        else:
+            choices.append((i, j))  # 내가 가로선을 놓을 수 있는 좌표들만 모아둔다. 후보 가로선들의 리스트.
+
+
+def reset_ladder():
+    global ladder
+    ladder = copy.deepcopy(ladder_copy)
+    # for i in range(1, h + 1):
+    #     for j in range(1, n):
+    #         ladder[i][j] = ladder_copy[i][j]
+
+
+def valid_coord(r: int, c: int) -> bool:
+    return 1 <= r <= h and 1 <= c <= n - 1
+
+
+def check(arr: list[list[int]], i: int) -> bool:
+    c = i
+    for r in range(1, h + 1):
+        if arr[r][c - 1]:
+            c = c - 1
+        elif arr[r][c]:
+            c = c + 1
+        else:
+            c = c
+    
+    return c == i
+
+
+def check_all() -> bool:
+    for i in range(1, n + 1):
+        if not check(ladder, i):
+            return False
+    return True
+
+
+def check_ladder_adding(ladder: list[list[int]], i: int, j: int):
+    if ladder[i][j] == True:
+        return False
+    elif ladder[i][j - 1] == True:
+        return False
+    elif ladder[i][j + 1] == True:
+        return False
+    else:
+        return True
+
+
+if check_all():
+    print(0)
+else:
+    ans = int(1e9)
+    
+    for p in range(len(choices)):
+        x, y = choices[p]
+        # if not check_ladder_adding(ladder, x, y):
+        #     continue
+        
+        ladder[x][y] = True
+        if check_all():
+            ans = min(ans, 1)
+
+        for q in range(p + 1, len(choices)):
+            x2, y2 = choices[q]
+            # if not check_ladder_adding(ladder, x2, y2):
+            #     continue
+            
+            ladder[x2][y2] = True
+            if check_all():
+                ans = min(ans, 2)
+            
+            for r in range(q + 1, len(choices)):
+                x3, y3 = choices[r]
+                # if not check_ladder_adding(ladder, x3, y3):
+                #     continue
+                
+                ladder[x3][y3] = True
+                if check_all():
+                    ans = min(ans, 3)
+                ladder[x3][y3] = False
+                
+            ladder[x2][y2] = False
+            
+        ladder[x][y] = False
+    
+    
+    if ans == int(1e9):
+        print(-1)
+    else:
+        print(ans)
+        
+    
+    
+
+
+
+
+
+
+
+# arr = [[False] * 5 for _ in range(5)]
+
+# arr[1][1] = True
+# arr[2][1] = True
+# arr[3][1] = False
+
+# print(check(arr, 1))
